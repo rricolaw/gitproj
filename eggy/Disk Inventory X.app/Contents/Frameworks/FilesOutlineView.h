a87adad60974c814df23b2483dd43b98c2ec3de0
RobertoRico <rricolaw@gmail.com>
2015-11-17 13:51:16 -0800

    add eggy folder

---
+++ eggy/Disk Inventory X.app/Contents/Frameworks/FilesOutlineView.h
@@ -0,0 +1,16 @@
+//
+//  FilesOutlineView.h
+//  Disk Inventory X new
+//
+//  Created by Tjark Derlien on 09.02.06.
+//  Copyright 2006 Tjark Derlien. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface FilesOutlineView : NSOutlineView {
+
+}
+
+@end
